893863d880b7ad6d00c01edf8eb8d19296b78ddb
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-23 20:14:41 -0600

    fix install requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="facehuggershield",
-    version="0.1.0",
+    version="0.1.1",
     author="Capsize LLC",
     description="",
     long_description=open("README.md", "r", encoding="utf-8").read(),
@@ -15,8 +15,8 @@ setup(
     packages=find_packages("src"),
     python_requires=">=3.10.0",
     install_requires=[
+        "defendatron==0.1.0",
     ],
     dependency_links=[
-        "defendatron==0.1.0",
     ],
 )
