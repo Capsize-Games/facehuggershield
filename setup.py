62e0adcd2f6473dd6a074633f0716e835251c590
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-23 20:26:07 -0600

    deleted obsolete file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="facehuggershield",
-    version="0.1.2",
+    version="0.1.3",
     author="Capsize LLC",
     description="",
     long_description=open("README.md", "r", encoding="utf-8").read(),
