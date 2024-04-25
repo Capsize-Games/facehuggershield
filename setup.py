ddcf4046f55527768dca01a132ec577ff39871bc
w4ffl35 <w4ffl35@ml1.net>
2024-04-25 11:40:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages("src"),
     python_requires=">=3.10.0",
     install_requires=[
-        "defendatron==0.1.1",
+        "defendatron==0.1.3",
     ],
     dependency_links=[
     ],
